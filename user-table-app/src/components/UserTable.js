b841fce1e435c2daa48af3ade115b55f35a36a18
Nisha Raghuwanshi <raghuwanshinisha1998@gmail.com>
2025-08-05 15:01:16 +0530

    Add Sorting functionality for ID and Name Columns

--- user-table-app/src/components/UserTable.js
+++ user-table-app/src/components/UserTable.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+
 
 const UserTable = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [sortBy, setSortBy] = useState('id');
+  const [sortOrder, setSortOrder] = useState('asc')
+
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-        setUsers(response.data);
+        setUsers(response?.data);
         setLoading(false);
       } catch (err) {
         setError('Failed to load users');
@@ -20,6 +25,31 @@ const UserTable = () => {
 
     fetchUsers();
   }, []);
+
+  const handleSort = (key) => {
+   const order = sortBy === key && sortOrder === 'asc' ? 'desc' : 'asc';
+    setSortBy(key);
+    setSortOrder(order);
+  }
+
+
+  const sortIcon = (key) => {
+    if (sortBy !== key) return null;
+    return sortOrder === 'asc' ? <FaArrowUp className="ms-1" /> : <FaArrowDown className="ms-1" />;
+  }
+
+   const sortedUsers = [...users].sort((a, b) => {
+    const valA = a[sortBy];
+    const valB = b[sortBy];
+
+    if (typeof valA === 'string') {
+      return sortOrder === 'asc'
+        ? valA.localeCompare(valB)
+        : valB.localeCompare(valA);
+    }
+    return sortOrder === 'asc' ? valA - valB : valB - valA;
+  });
+
 
   return (
     <div className="container mt-5">
@@ -43,14 +73,18 @@ const UserTable = () => {
         <table className="table table-bordered table-striped">
           <thead className="table-dark">
             <tr>
-              <th>ID</th>
-              <th>Name</th>
+              <th style={{ cursor: 'pointer' }} onClick={() => handleSort('id')}>
+                ID {sortIcon('id')}
+              </th>
+              <th style={{ cursor: 'pointer' }} onClick={() => handleSort('name')}>
+                Name {sortIcon('name')}
+              </th>
               <th>Address (City & Zipcode)</th>
               <th>Company</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {sortedUsers.map((user) => (
               <tr key={user.id}>
                 <td>{user.id}</td>
                 <td>{user.name}</td>
